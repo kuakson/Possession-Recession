09f97c566ff8e829e26502bd503ed2dcfb0f44b0
Mikołaj Markowski <mik.markowski@gmail.com>
2023-06-12 19:48:21 +0000

    adding folders for assets

--- scripts/player.gd
+++ scripts/player.gd
@@ -3,6 +3,7 @@ extends AnimatedSprite2D
 
 @export var speed = 300
 
+
 func _physics_process(delta):
 	var velocity = Vector2(0, 0)
 	if Input.is_action_pressed("move down"):
@@ -13,3 +14,4 @@ func _physics_process(delta):
 		velocity.x = speed
 	if Input.is_action_pressed("move left"):
 		velocity.y = -speed
+	move_and_slide()
