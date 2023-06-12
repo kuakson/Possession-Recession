b8a0331d0b9b7e1bb3e7cde31080f9ebc79f0f99
Mikołaj Markowski <mik.markowski@gmail.com>
2023-06-12 19:43:13 +0000

    first push

---
+++ scripts/player.gd
@@ -0,0 +1,15 @@
+extends AnimatedSprite2D
+
+
+@export var speed = 300
+
+func _physics_process(delta):
+	var velocity = Vector2(0, 0)
+	if Input.is_action_pressed("move down"):
+		velocity.y = speed
+	if Input.is_action_pressed("move up"):
+		velocity.y = -speed
+	if Input.is_action_pressed("move right"):
+		velocity.x = speed
+	if Input.is_action_pressed("move left"):
+		velocity.y = -speed
