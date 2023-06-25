25b49cc8d6591700a63137515a03cfeb59c47e01
Mikołaj Markowski <mik.markowski@gmail.com>
2023-06-25 16:05:24 +0000

    added controls

--- scripts/player.gd
+++ scripts/player.gd
@@ -1,17 +1,44 @@
-extends AnimatedSprite2D
+extends CharacterBody2D
 
 
 @export var speed = 300
+@export var dashSpeed = 1000
+@export var dashDuration = 0.5
 
-
+var dashTimer = 0
+var dashDirection = Vector2.ZERO
+ 
 func _physics_process(delta):
-	var velocity = Vector2(0, 0)
-	if Input.is_action_pressed("move down"):
-		velocity.y = speed
-	if Input.is_action_pressed("move up"):
-		velocity.y = -speed
-	if Input.is_action_pressed("move right"):
-		velocity.x = speed
-	if Input.is_action_pressed("move left"):
-		velocity.y = -speed
-	#move_and_slide()
+	#movement
+	var direction = Input.get_vector("move left", "move right", "move up", "move down").normalized()
+	if direction:
+		velocity = direction * speed
+	else:
+		velocity = Vector2.ZERO
+	move_and_slide()	
+	#dashing for possession
+	if dashTimer > 0:
+		dashTimer -= delta
+		move_and_collide(dashDirection * dashSpeed * delta)
+	else:
+		var inputVector = Vector2.ZERO
+		inputVector.x = Input.get_action_strength("ui_right") - Input.get_action_strength("ui_left")
+		inputVector.y = Input.get_action_strength("ui_down") - Input.get_action_strength("ui_up")
+		inputVector = inputVector.normalized()
+		move_and_slide()
+
+	if Input.is_action_just_pressed("possess"):
+		possession()
+		
+	if Input.is_action_just_pressed("attack"):
+		attack()
+		
+func possession(): #RMB
+	print("I'm possessing")
+	var mousePosition = get_global_mouse_position()
+	var playerPosition = global_position
+	dashDirection = (mousePosition - playerPosition).normalized()
+	dashTimer = dashDuration
+   
+func attack(): #LMB
+	print("I'm attacking")
